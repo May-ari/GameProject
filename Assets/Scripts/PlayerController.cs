12a2e8c12008d7f6e586bf8d755623434b76fd70
Daniil Riznyk <danila.riznyk@gmail.com>
2024-01-09 20:00:37 +0100

    Added new animaions and attack mechanic

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,14 +5,52 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public enum PlayerStates
+    {
+        IDLE,
+        RUN,
+        ATTACK
+    }
+    PlayerStates CurrentState
+    {
+        set
+        {
+            if (stateLock == false)
+            {
+                currentState = value;
+                switch (currentState)
+                {
+                    case PlayerStates.IDLE:
+                        animator.Play("IDLE");
+                        canMove = true;
+                        break;
+
+                    case PlayerStates.RUN:
+                        animator.Play("RUN");
+                        canMove = true;
+                        break;
+
+                    case PlayerStates.ATTACK:
+                        animator.Play("ATTACK");
+                        stateLock = true;
+                        canMove = false;
+                        break;
+                }
+            }
+        }
+    }
     public float moveSpeed = 1f;
     public float collisionOffset = 0.05f;
     public ContactFilter2D movementFilter;
 
-    Vector2 movementInput;
+    Vector2 movementInput = Vector2.zero;
     SpriteRenderer spriteRenderer;
     Rigidbody2D rb;
     Animator animator;
+    PlayerStates currentState;
+
+    bool stateLock = false;
+    bool canMove = true;
     
     List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
 
@@ -21,10 +59,6 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
-    }
-    void Update()
-    {
-        
     }
     private void FixedUpdate()
     {
@@ -40,21 +74,14 @@ public class PlayerController : MonoBehaviour
             {
                 success = TryMove(new Vector2(0, movementInput.y));
             }
-            animator.SetBool("isMoving", success);
+           // animator.SetBool("isMoving", success);
         } 
-        else
-        {
-            animator.SetBool("isMoving", false);
-        }
+        //else
+        //{
+        //    animator.SetBool("isMoving", false);
+        //}
 
-        if (movementInput.x < 0)
-        {
-            spriteRenderer.flipX = true;
-        }
-        else if (movementInput.x > 0)
-        {
-            spriteRenderer.flipX = false;
-        }
+        
     }
 
     private bool TryMove(Vector2 direction)
@@ -82,5 +109,33 @@ public class PlayerController : MonoBehaviour
     void OnMove(InputValue movementValue)
     {
         movementInput = movementValue.Get<Vector2>();
+        //Update Animator for sprite direction
+        if (canMove && movementInput != Vector2.zero)
+        {
+            CurrentState = PlayerStates.RUN;
+            animator.SetFloat("xMove", movementInput.x);
+            animator.SetFloat("yMove", movementInput.y);
+        
+        //Set facing left or right
+        if (movementInput.x < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (movementInput.x > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        }else{
+            CurrentState = PlayerStates.IDLE;
+        }
+    }
+    void OnFire()
+    {
+        CurrentState = PlayerStates.ATTACK;
+    }
+    void OnAttackFinished()
+    {
+        stateLock = false;
+        CurrentState = PlayerStates.IDLE;
     }
 }
